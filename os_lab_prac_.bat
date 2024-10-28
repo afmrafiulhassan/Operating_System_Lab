17538b9fadc3ae4d41a1958dcd352a1c9e437d8c
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-10-29 00:08:21 +0600

    Add files via upload

---
+++ os_lab_prac_.bat
@@ -0,0 +1,24 @@
+C:\Users\Zayyan>cd documents
+C:\Users\Zayyan\Documents>mkdir MyFolder
+C:\Users\Zayyan\Documents>cd MyFolder
+C:\Users\Zayyan\Documents\MyFolder>echo > file1.txt
+C:\Users\Zayyan\Documents\MyFolder>echo "Hello, World!" > file1.txt
+C:\Users\Zayyan\Documents\MyFolder>type file1.txt
+C:\Users\Zayyan\Documents\MyFolder>echo > file2.txt
+C:\Users\Zayyan\Documents\MyFolder>(for %i in ("1st Line" "2nd Line" "3rd Line") do @echo %i) > file2.txt
+C:\Users\Zayyan\Documents\MyFolder>type file2.txt
+C:\Users\Zayyan\Documents\MyFolder>mkdir SubFolder
+C:\Users\Zayyan\Documents\MyFolder>cd SubFolder
+C:\Users\Zayyan\Documents\MyFolder\SubFolder>echo "Hello, World!" > file1.txt
+C:\Users\Zayyan\Documents\MyFolder\SubFolder>type file1.txt
+C:\Users\Zayyan\Documents\MyFolder\SubFolder>cd ..
+C:\Users\Zayyan\Documents\MyFolder>del file2.txt
+C:\Users\Zayyan\Documents\MyFolder>for %i in (a.txt b.txt c.txt) do echo "Sample Text" > %i
+C:\Users\Zayyan\Documents\MyFolder>del *.txt
+C:\Users\Zayyan\Documents\MyFolder>rmdir /s SubFolder
+C:\Users\Zayyan\Documents\MyFolder>date /t
+C:\Users\Zayyan\Documents\MyFolder>time /t
+C:\Users\Zayyan\Documents\MyFolder>ipconfig
+C:\Users\Zayyan\Documents\MyFolder>echo "Rafi" > a.txt
+C:\Users\Zayyan\Documents\MyFolder>echo > b.txt
+C:\Users\Zayyan\Documents\MyFolder>copy a.txt b.txt
