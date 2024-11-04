08dbaed91e1eef224aceb9ecc9e7af47ac97a30d
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-05 00:11:08 +0600

    Add files via upload

---
+++ lab_task_2.sh
@@ -0,0 +1,138 @@
+~$ pwd
+/home/user
+~$ uname
+Linux
+~$ mkdir Rafiul
+~$ cd Rafiul
+~/Rafiul$ ls
+~/Rafiul$ ls -a
+.  ..
+~/Rafiul$ touch aiub.txt
+~/Rafiul$ touch Bangladesh > aiub.txt
+~/Rafiul$ cat aiub.txt
+~/Rafiul$ echo Bangladesh > aiub.txt
+~/Rafiul$ cat aiub.txt
+Bangladesh
+~/Rafiul$ echo Pakistan >> aiub.txt
+~/Rafiul$ type aiub.txt
+bash: type: aiub.txt: not found
+~/Rafiul$ cat aiub.txt
+Bangladesh
+Pakistan
+~/Rafiul$ touch brac.txt nsu.txt ewu.txt
+~/Rafiul$ ls *.txt
+aiub.txt  brac.txt  ewu.txt  nsu.txt
+~/Rafiul$ mkdir -p Zayyan/Nusaiba
+~/Rafiul$ cd
+~$ cd Rafiul
+~/Rafiul$ cd Zayyan
+~/Rafiul/Zayyan$ cd Nusaiba
+~/Rafiul/Zayyan/Nusaiba$ uname -r
+5.15.0-1046-gcp
+~/Rafiul/Zayyan/Nusaiba$ uname -m
+x86_64
+~/Rafiul/Zayyan/Nusaiba$ touch .seu.txt
+~/Rafiul/Zayyan/Nusaiba$ pwd
+/home/user/Rafiul/Zayyan/Nusaiba
+~/Rafiul/Zayyan/Nusaiba$ pwd >> New.txt
+~/Rafiul/Zayyan/Nusaiba$ cat New.txt
+/home/user/Rafiul/Zayyan/Nusaiba
+~/Rafiul/Zayyan/Nusaiba$ uname
+Linux
+~/Rafiul/Zayyan/Nusaiba$ uname >> Log.txt
+~/Rafiul/Zayyan/Nusaiba$ cat Log.txt
+Linux
+~/Rafiul/Zayyan/Nusaiba$ history
+    1  date
+    2  cal -y
+    3  help.man
+    4  help man
+    5  man cal
+    6  help
+    7  uname -r
+    8  clear
+    9  date
+   10  cal -y
+   11  help.man
+   12  help man
+   13  help
+   14  uname -r
+   15  pwd
+   16  ls
+   17  dir
+   18  mkdir Rafi
+   19  ls
+   20  ls -l
+   21  ls la
+   22  ls -la
+   23  pwd
+   24  cd Rafi
+   25  pwd
+   26  touch a.txt
+   27  ls -la
+   28  echo Dima > a.txt
+   29  cat a.txt
+   30  man ls
+   31  cd ..
+   32  dir
+   33  ls *.txt
+   34  cd Rafo
+   35  cd Rafi
+   36  ls *a.txt
+   37  echo Dima1 > b.txt
+   38  echo ima2 > c.txt
+   39  ls *a.txt
+   40  ls *.txt
+   41  ls >> info.txt
+   42  cat info.txt
+   43  cat > info2.txt
+   44  ls
+   45  ls >> info3.txt
+   46  cat info3.txt 
+   47  cal > info5.txt
+   48  cat info5.txt
+   49  pwd
+   50  uname -r
+   51  uname -m
+   52  mkdir Rafi
+   53  clear
+   54  pwd
+   55  uname -m
+   56  uname
+   57  clear
+   58  pwd
+   59  uname
+   60  mkdir Rafiul\
+   61  clear
+   62  pwd
+   63  uname
+   64  mkdir Rafiul
+   65  cd Rafiul
+   66  ls
+   67  ls -a
+   68  touch aiub.txt
+   69  touch Bangladesh > aiub.txt
+   70  cat aiub.txt
+   71  echo Bangladesh > aiub.txt
+   72  cat aiub.txt
+   73  echo Pakistan >> aiub.txt
+   74  type aiub.txt
+   75  cat aiub.txt
+   76  touch brac.txt nsu.txt ewu.txt
+   77  ls *.txt
+   78  mkdir -p Zayyan/Nusaiba
+   79  cd
+   80  cd Rafiul
+   81  cd Zayyan
+   82  cd Nusaiba
+   83  uname -r
+   84  uname -m
+   85  touch .seu.txt
+   86  pwd
+   87  pwd >> New.txt
+   88  cat New.txt
+   89  uname
+   90  uname >> Log.txt
+   91  cat Log.txt
+   92  history
+   ~$ ~$ Rafiul
